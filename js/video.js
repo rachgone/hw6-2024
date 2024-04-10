3adbb9340e1eb2641b029010e1aa93c1315fab73
rachgone <143525609+rachgone@users.noreply.github.com>
2024-04-09 20:59:43 -0400

    Add files via upload
    js updated

--- js/video.js
+++ js/video.js
@@ -17,9 +17,11 @@ var video = document.getElementById('player1');
 video.autoplay = false;
 video.loop = false;
 
-
 var volumeSpan = document.getElementById('volume');
-volumeSpan.textContent = video.volume * 100 + '%';
+document.getElementById('play').addEventListener('click', function () {
+	video.play();
+	volumeSpan.textContent = (video.volume * 100).toFixed(0) + '%';
+});
 
 
 document.getElementById('play').addEventListener('click', function () {
